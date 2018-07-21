b7f3072d82a7fbb48c77fe6f92a3575294888a1a
Wajdi Jurry <creations.forum@gmail.com>
2018-07-21 10:35:47 +0000

    Base Project

---
+++ app/modules/frontend/controllers/ControllerBase.php
@@ -0,0 +1,9 @@
+<?php
+namespace Shop_users\Modules\Frontend\Controllers;
+
+use Phalcon\Mvc\Controller;
+
+class ControllerBase extends Controller
+{
+
+}
---
+++ app/modules/frontend/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Shop_users\Modules\Frontend\Controllers;
+
+class IndexController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
